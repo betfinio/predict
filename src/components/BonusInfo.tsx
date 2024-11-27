854793bf3c9500fb8e4a46747d1f8c4af05794da
Monster <bf.monster@proton.me>
2024-11-27 16:11:21 +0100

    feat: updated to components

--- src/components/BonusInfo.tsx
+++ src/components/BonusInfo.tsx
@@ -3,7 +3,7 @@ import i18n from '@/src/i18n.ts';
 import { useCurrentRound, usePool, useRoundBets } from '@/src/lib/query';
 import type { Game } from '@/src/lib/types';
 import { valueToNumber } from '@betfinio/abi';
-import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from 'betfinio_app/tooltip';
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from '@betfinio/components/ui';
 import { CircleHelp } from 'lucide-react';
 import type { FC } from 'react';
 import { Trans, useTranslation } from 'react-i18next';
@@ -38,7 +38,7 @@ const BonusInfo: FC<{ game: Game }> = ({ game }) => {
 
 	return (
 		<div className={'h-full'}>
-			<div className={'bg-primaryLight rounded-md p-2 lg:p-4 flex flex-col relative  justify-between gap-2 lg:gap-4 '}>
+			<div className={'bg-background-light rounded-md p-2 lg:p-4 flex flex-col relative  justify-between gap-2 lg:gap-4 '}>
 				<div className={'relative h-[200px] w-full'}>
 					<BonusChart bonuses={bonuses} />
 				</div>
@@ -48,14 +48,14 @@ const BonusInfo: FC<{ game: Game }> = ({ game }) => {
 							<CircleHelp className={'w-5 h-5'} />
 						</TooltipTrigger>
 						<TooltipContent>
-							<div className={'text-xs text-gray-500 italic'}>
+							<div className={'text-xs text-muted-foreground italic'}>
 								<div className={'text-center'}>
 									<Trans
 										t={t}
 										i18nKey={'bonus.explain'}
 										values={{ bonus: (valueToNumber(pool.short + pool.long) / 25).toLocaleString() }}
 										i18n={i18n}
-										components={{ b: <b className={'text-yellow-400 font-medium'} /> }}
+										components={{ b: <b className={'text-secondary-foreground font-medium'} /> }}
 									/>
 								</div>
 								<p className={'text-center font-semibold'}>{t('bonus.info')}</p>
--- src/components/CoinInfo.tsx
+++ src/components/CoinInfo.tsx
@@ -1,6 +1,6 @@
-import { BetValue } from 'betfinio_app/BetValue';
-import { Drawer, DrawerContent, DrawerTitle, DrawerTrigger } from 'betfinio_app/drawer';
-import cx from 'clsx';
+import { cn } from '@betfinio/components/lib';
+import { BetValue } from '@betfinio/components/shared';
+import { Drawer, DrawerContent, DrawerTitle, DrawerTrigger } from '@betfinio/components/ui';
 import { motion } from 'framer-motion';
 import { ChartBarIcon } from 'lucide-react';
 import type { FC } from 'react';
@@ -11,10 +11,14 @@ const CoinInfo: FC<{ bets: number; volume: bigint; staking: bigint; diff: number
 
 	return (
 		<div className={'lg:hidden'}>
-			<Drawer>
+			<Drawer fadeFromIndex={0} snapPoints={[]}>
 				<DrawerTrigger asChild>
-					<motion.div className={'flex flex-col items-center justify-center cursor-pointer text-yellow-400 hover:text-yellow-400 lg:text-white duration-300'}>
-						<ChartBarIcon className={'text-yellow-400 w-6'} />
+					<motion.div
+						className={
+							'flex flex-col items-center justify-center cursor-pointer text-secondary-foreground hover:text-secondary-foreground lg:text-foreground duration-300'
+						}
+					>
+						<ChartBarIcon className={'text-secondary-foreground w-6'} />
 						<span className={'hidden sm:inline text-xs'}>{t('stats')}</span>
 					</motion.div>
 				</DrawerTrigger>
@@ -33,14 +37,14 @@ const StatModal: FC<{ bets: number; volume: bigint; staking: bigint; diff: numbe
 	const { t } = useTranslation('predict');
 
 	return (
-		<div className={'p-4 w-full lg:w-[300px] flex flex-col gap-4 text-white'}>
+		<div className={'p-4 w-full lg:w-[300px] flex flex-col gap-4 text-foreground'}>
 			<div className={'flex justify-between'}>
 				<span className={'text-sm'}>{t('tile.24change')}</span>
 				<span
-					className={cx('font-semibold text-sm lg:text-lg', {
-						'text-green-500': diff > 0,
-						'text-red-500': diff < 0,
-						'text-gray-500': diff === 0,
+					className={cn('font-semibold text-sm lg:text-lg', {
+						'text-success': diff > 0,
+						'text-destructive': diff < 0,
+						'text-muted-foreground': diff === 0,
 					})}
 				>
 					{diff > 0 ? '+' : ''}
