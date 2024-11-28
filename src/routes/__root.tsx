09577262ec1e0a347693bb860dc7522f31e5d480
Plane <bf.plane@proton.me>
2024-11-28 19:25:39 +0100

    feat: add version validator

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,3 +1,4 @@
+import { VersionValidation } from '@/src/components/VersionValidation.tsx';
 import instance from '@/src/i18n.ts';
 import { Toaster } from '@betfinio/components/ui';
 import { createRootRoute } from '@tanstack/react-router';
@@ -7,6 +8,7 @@ export const Route = createRootRoute({
 	component: () => (
 		<Root id={'predict'} instance={instance}>
 			<Toaster />
+			<VersionValidation repository={'affiliate'} branch={import.meta.env.PUBLIC_BRANCH} current={import.meta.env.PUBLIC_DEPLOYED} />
 		</Root>
 	),
 });
